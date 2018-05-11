ee29983e3ead187756a81f3d1444d6efe103958b
DanielJDufour <daniel.j.dufour@gmail.com>
2018-05-11 01:39:14 +0000

    added additional test

--- data/setup_data.sh
+++ data/setup_data.sh
@@ -1,3 +1,5 @@
 wget https://s3.amazonaws.com/georaster/iso.xml -O iso.xml
 
 wget https://s3.amazonaws.com/georaster/geonode_atlanteil.tif -O geonode_atlanteil.tif
+
+wget https://s3.amazonaws.com/georaster/rgb_raster.tif -O rgb_raster.tif
