6b8a86ea9cc6c3ca4091695d356f88b9db524df3
Tim Welch <tim.j.welch@gmail.com>
2021-06-27 15:10:35 -0700

    add module declaration

--- src/georaster.d.ts
+++ src/georaster.d.ts
@@ -1,39 +1,42 @@
-export interface Georaster {
-  /** raster values.  first dimension is raster band, remainder is 2D array of cell values */
-  values: number[][][];
-  /** raster height in units of projection */
-  height: number;
-  /** raster width in units of projection */
-  width: number;
-  /** raster height in pixels */
-  pixelHeight: number;
-  /** raster width in pixels */
-  pixelWidth: number;
-  /** Projection identifier */
-  projection: unknown;
-  /** left boundary, in units of projection*/
-  xmin: number;
-  /** right boundary, in units of projection */
-  xmax: number;
-  /** top boundary (image y-axis is inverse of cartesian), in units of projection */
-  ymin: number;
-  /** bottom boundary (image y-axis is inverse of cartesian), in units of projection */
-  ymax: number;
-  /** cell value representing "no data" in raster */
-  noDataValue: number;
-  /** number of raster bands */
-  numberOfRasters: number;
-  /** Minimum cell value for each raster band.  Indexed by band number */
-  mins: number[]
-  /** Maximum cell value for each raster band.  Indexed by band number */
-  maxs: number[]
-  /** difference between max and min for each raster band.  Indexed by band number */
-  ranges: number[]
+declare module georaster {
+
+  export interface Georaster {
+    /** raster values.  first dimension is raster band, remainder is 2D array of cell values */
+    values: number[][][];
+    /** raster height in units of projection */
+    height: number;
+    /** raster width in units of projection */
+    width: number;
+    /** raster height in pixels */
+    pixelHeight: number;
+    /** raster width in pixels */
+    pixelWidth: number;
+    /** Projection identifier */
+    projection: unknown;
+    /** left boundary, in units of projection*/
+    xmin: number;
+    /** right boundary, in units of projection */
+    xmax: number;
+    /** top boundary (image y-axis is inverse of cartesian), in units of projection */
+    ymin: number;
+    /** bottom boundary (image y-axis is inverse of cartesian), in units of projection */
+    ymax: number;
+    /** cell value representing "no data" in raster */
+    noDataValue: number;
+    /** number of raster bands */
+    numberOfRasters: number;
+    /** Minimum cell value for each raster band.  Indexed by band number */
+    mins: number[]
+    /** Maximum cell value for each raster band.  Indexed by band number */
+    maxs: number[]
+    /** difference between max and min for each raster band.  Indexed by band number */
+    ranges: number[]
+  }
+
+  export type GeorasterMetadata = Pick<Georaster, 'noDataValue' | 'projection' | 'xmin' | 'ymax' | 'pixelWidth' | 'pixelHeight'>
+
+  export function parseGeoraster(data: object | string | Buffer | ArrayBuffer | number[][][],
+      metadata: GeorasterMetadata,
+      debug: boolean
+  ): Promise<Georaster>
 }
-
-export type GeorasterMetadata = Pick<Georaster, 'noDataValue' | 'projection' | 'xmin' | 'ymax' | 'pixelWidth' | 'pixelHeight'>
-
-export function parseGeoraster(data: object | string | Buffer | ArrayBuffer | number[][][],
-    metadata: GeorasterMetadata,
-    debug: boolean
-): Promise<Georaster>
