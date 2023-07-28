fdffef1f2037929c8ae270e78bfe1205866cdebc
Jim Phillips <jcphill@users.noreply.github.com>
2023-07-28 13:21:34 -0500

    avoid worker data copy when returning values typed arrays

--- src/worker.js
+++ src/worker.js
@@ -7,11 +7,14 @@ const window = self;
 onmessage = e => {
   const data = e.data;
   parseData(data).then(result => {
-    if (result._data instanceof ArrayBuffer) {
-      postMessage(result, [result._data]);
-    } else {
-      postMessage(result);
+    let transferBuffers = [];
+    if ( result.values && (result.values[0][0].buffer instanceof ArrayBuffer) ) {
+        transferBuffers = result.values.flat().map(r => r.buffer);
     }
+    if (result._data instanceof ArrayBuffer) {
+      transferBuffers.push(result._data);
+    }
+    postMessage(result, transferBuffers);
     close();
   });
 };
